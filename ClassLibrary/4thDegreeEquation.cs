141fdb519d4ed1d3a445295e723d72796c2bbe55
Ghornon <szymon198198@gmail.com>
2022-05-18 09:59:44 +0200

    Add precision

--- ClassLibrary/4thDegreeEquation.cs
+++ ClassLibrary/4thDegreeEquation.cs
@@ -38,27 +38,27 @@ namespace ClassLibrary
         {
             //Obliczamy F,G,H
 
-            double f = (c / a) - ((3 * Math.Pow(b, 2)) / (8 * Math.Pow(a, 2)));
-            double g = (d / a) + (Math.Pow(b, 3) / (8 * Math.Pow(a, 3))) - ((b * c) / (2 * Math.Pow(a, 2)));
-            double h = (e / a) - ((3 * Math.Pow(b, 4)) / (256 * Math.Pow(a, 4))) + ((Math.Pow(b, 2) * c) / (16 * Math.Pow(a, 3))) - ((b * d) / (4 * Math.Pow(a, 2)));
+            double f = (c / a) - ((3 * Math.Pow(b, 2.0)) / (8 * Math.Pow(a, 2.0)));
+            double g = (d / a) + (Math.Pow(b, 3.0) / (8.0 * Math.Pow(a, 3.0))) - ((b * c) / (2.0 * Math.Pow(a, 2.0)));
+            double h = (e / a) - ((3 * Math.Pow(b, 4.0)) / (256.0 * Math.Pow(a, 4.0))) + ((Math.Pow(b, 2.0) * c) / (16.0 * Math.Pow(a, 3.0))) - ((b * d) / (4.0 * Math.Pow(a, 2.0)));
 
             //Definiujemy wartości Argumenty-PRIM
 
-            double aprim = 1;
-            double bprim = f / 2;
-            double cprim = (Math.Pow(f, 2) - (4 * h)) / 16;
-            double dprim = -(Math.Pow(g, 2) / 64);
+            double aprim = 1.0;
+            double bprim = f / 2.0;
+            double cprim = (Math.Pow(f, 2.0) - (4.0 * h)) / 16.0;
+            double dprim = -(Math.Pow(g, 2.0) / 64.0);
 
             Console.WriteLine(aprim + "y^3 + " + bprim + "y^2 + " + cprim + "y + " + dprim + " = 0");
 
-            double w = -(bprim / (3 * aprim));
-            double p = ((3 * aprim * Math.Pow(w, 2)) + (2 * bprim * w) + cprim) / aprim;
-            double q = ((aprim * Math.Pow(w, 3)) + (bprim * Math.Pow(w, 2)) + (cprim * w) + dprim) / aprim;
+            double w = -(bprim / (3.0 * aprim));
+            double p = ((3.0 * aprim * Math.Pow(w, 2.0)) + (2.0 * bprim * w) + cprim) / aprim;
+            double q = ((aprim * Math.Pow(w, 3.0)) + (bprim * Math.Pow(w, 2.0)) + (cprim * w) + dprim) / aprim;
 
 
             //Obliczamy deltę i porównujemy ją z zerem
 
-            double delta = (Math.Pow(q, 2) / 4) + (Math.Pow(p, 3) / 27);
+            double delta = (Math.Pow(q, 2.0) / 4.0) + (Math.Pow(p, 3.0) / 27.0);
             Console.WriteLine("Delta = " + delta);
 
             if (delta > 0)
@@ -83,9 +83,7 @@ namespace ClassLibrary
             double x1 = u + v + w;
             double x2 = (-0.5 * (u + v)) + w;
             double x3 = x2;
-            double urojona = (Math.Sqrt(3) / 2.0) * (u - v);
-
-            Console.WriteLine($"{x1}, {x2}, {x3}");
+            double urojona = (Math.Sqrt(3.0) / 2.0) * (u - v);
 
             Console.WriteLine("urojona: " + urojona);
 
@@ -98,8 +96,8 @@ namespace ClassLibrary
 
         static void DeltaRowna(double w, double p, double q, double delta, double g, double a, double b)
         {
-            double x1 = w - 2 * Math.Cbrt(q / 2);
-            double x23 = w + Math.Cbrt(q / 2);
+            double x1 = w - 2.0 * Math.Cbrt(q / 2.0);
+            double x23 = w + Math.Cbrt(q / 2.0);
 
             Console.WriteLine($"{x1}, {x23}");
 
@@ -110,7 +108,7 @@ namespace ClassLibrary
         {
             Console.WriteLine("Trzy pierwiastki rzeczyswiste.");
 
-            double fi = Math.Acos((3 * q) / (2 * p * Math.Sqrt(-p / 3.0)));
+            double fi = Math.Acos((3.0 * q) / (2.0 * p * Math.Sqrt(-p / 3.0)));
 
             double x1 = (w + (2 * Math.Sqrt(-p / 3.0)) * Math.Cos(fi / 3.0));
             double x2 = (w + 2 * Math.Sqrt(-p / 3.0) * Math.Cos(fi / 3.0 + (2.0 * Math.PI) / 3.0));
@@ -128,14 +126,12 @@ namespace ClassLibrary
                 double P, Q, R, S;
                 P = checkSquareRoot(y1, 1.0 / 2.0);
                 Q = checkSquareRoot(y2, 1.0 / 2.0);
-                R = -(g / (8 * P * Q));
-                S = b / (4 * a);
+                R = -(g / (8.0 * P * Q));
+                S = b / (4.0 * a);
                 Console.WriteLine("x1 = " + (P + Q) + " + " + (R - S) + "i");
                 Console.WriteLine("x2 = " + (P - Q) + " + " + (-R - S) + "i");
                 Console.WriteLine("x3 = " + (-P + Q) + " + " + (-R - S) + "i");
                 Console.WriteLine("x4 = " + (-P - Q) + " + " + (R - S) + "i");
-
-                Console.WriteLine($"{P}, {Q}, {R}, {S}");
 
 
             }
@@ -146,8 +142,8 @@ namespace ClassLibrary
                 double P, Q, R, S;
                 P = checkSquareRoot(y1, 1.0 / 2.0);
                 Q = checkSquareRoot(y2, 1.0 / 2.0);
-                R = -(g / (8 * P * Q));
-                S = b / (4 * a);
+                R = -(g / (8.0 * P * Q));
+                S = b / (4.0 * a);
                 Console.WriteLine("x1 = " + (P + Q) + " + " + (R - S) + "i");
                 Console.WriteLine("x2 = " + (P - Q) + " + " + (-R - S) + "i");
                 Console.WriteLine("x3 = " + (-P + Q) + " + " + (-R - S) + "i");
@@ -161,8 +157,8 @@ namespace ClassLibrary
                 double P, Q, R, S;
                 P = checkSquareRoot(y1, 1.0 / 2.0);
                 Q = checkSquareRoot(y2, 1.0 / 2.0);
-                R = -(g / (8 * P * Q));
-                S = b / (4 * a);
+                R = -(g / (8.0 * P * Q));
+                S = b / (4.0 * a);
                 Console.WriteLine("x1 = " + (P + Q) + " + " + (R - S) + "i");
                 Console.WriteLine("x2 = " + (P - Q) + " + " + (-R - S) + "i");
                 Console.WriteLine("x3 = " + (-P + Q) + " + " + (-R - S) + "i");
